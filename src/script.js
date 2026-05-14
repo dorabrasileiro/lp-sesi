ea2c7c357050351c2baa7ecc7cbe2e90d0356a9e
dorabrasileiro <dora2020br@gmail.com>
2026-05-14 12:35:51 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
 window.addEventListener('scroll', () => {
     const nav = document.querySelector('nav')
+    const footer = document.querySelector('footer')
 
     if(window.scrollY > 0){
         nav.classList.add('sc');
+        footer.classList.add('ft')
+
     }else{
         nav.classList.remove('sc');
+        footer.classList.remove('ft')
     }
 })
 
